41e90f7db06fba9d9bd834d83399dfadee9cbe20
Jozef Hudec <jojohudec27@gmail.com>
2022-10-24 20:28:13 +0200

    Create models Airport and Flight

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+airport_codes = ['ATL', 'DFW', 'DEN', 'ORD', 'LAX', 'CLT', 'MCO', 'LAS', 'PHX', 'MIA']
+
+airport_codes.each do |airport_code|
+  Airport.create(code: airport_code)
+end
